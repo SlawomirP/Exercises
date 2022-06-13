37570c393c57e5c555f2903a151c6c0b20d2361c
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-06-13 17:36:06 +0200

    Add files via upload

--- ex4_15/Ex.java
+++ ex4_15/Ex.java
@@ -111,7 +111,57 @@ public class Ex {
 
         System.out.println("The biggest number is " + theBiggest(firstN,secondN,thirdN));
 
-    }
+        System.out.println();
+
+        //4.15.5
+        System.out.println("---------Choose your month---------");
+        System.out.println("Write number of oyur month...");
+
+        int month = getInt();
+
+        switch (month) {
+            case 1:
+                System.out.println("-January-");
+                break;
+            case 2:
+                System.out.println("-February-");
+                break;
+            case 3:
+                System.out.println("-March-");
+                break;
+            case 4:
+                System.out.println("-April-");
+                break;
+            case 5:
+                System.out.println("-May-");
+                break;
+            case 6:
+                System.out.println("-June-");
+                break;
+            case 7:
+                System.out.println("-July-");
+                break;
+            case 8:
+                System.out.println("-August-");
+                break;
+            case 9:
+                System.out.println("-September-");
+                break;
+            case 10:
+                System.out.println("-October-");
+                break;
+            case 11:
+                System.out.println("-November-");
+                break;
+            case 12:
+                System.out.println("-December-");
+                break;
+            default :
+                System.out.println("Unknown");
+            }
+        }
+
+
 
     static int getInt() {
         return new Scanner(System.in).nextInt();
