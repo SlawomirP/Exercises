5945dc13e32227e76f2af3f5bc6f03ca0d354b48
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-06-16 10:44:09 +0200

    Add files via upload

--- ex6_9/Ex69.java
+++ ex6_9/Ex69.java
@@ -1,5 +1,6 @@
 package ex6_9;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //6.9 Zadania
@@ -36,67 +37,120 @@ import java.util.Scanner;
 public class Ex69 {
     public static void main(String[] args) {
 
-        // 6.9.1
+//        // 6.9.1
+//
+//        int [] tabl1 = {1,2,3,4,5,6,7,8,9,10};
+//        int [] tabl2 = {11,12,13,14,15,16,17,18,19,20,21};
+//
+//        for(int i = 0; i < tabl1.length; i += 2){
+//            System.out.print(tabl1[i] + " ");
+//        }
+//        System.out.println();
+//        for(int i = 0; i < tabl2.length; i += 2){
+//            System.out.print(tabl2[i] + " ");
+//        }
+//
+//        System.out.println();
+//
+//        // 6.9.2
+//        int maxValue = tabl1[0];
+//        for (int i : tabl1){
+//            if (maxValue < i){
+//                maxValue = i;
+//            }
+//        }
+//        System.out.println(maxValue + " is the biggest number");
+//
+//        System.out.println();
+//
+//        // 6.9.3
+//
+//        String [] tabl3 = {"ala", "ola", "bartek", "tomek"};
+//
+//        for(String i : tabl3){
+//            System.out.println(i.toUpperCase() + " ");
+//        }
+//
+//        System.out.println();
+//
+//        // 6.9.4
+//
+//        String [] tabl4 = {"ala", "ma", "kota", "i", "psa"};
+//
+//        for (int i = tabl4.length - 1; i >= 0 ; i--){
+//            for(int j = tabl4[i].length() - 1; j >= 0; j--){
+//                System.out.print(tabl4[i].charAt(j));
+//            }
+//            System.out.print(" ");
+//        }
+//
+//        System.out.println();
+//
+//        // 6.9.5
+//
+//        int [] tabl5 = new int [8];
+//
+//        for(int i = 0 ; i < tabl5.length; i++){
+//            System.out.println("Write your number ...");
+//            tabl5[i] = getInt();
+//        }
+//
+//        Arrays.sort(tabl5);
+//
+//        for(int i : tabl5){
+//            System.out.print(i + " ");
+//        }
+//
+//        System.out.println();
+//
+        // 6.9.6
 
-        int [] tabl1 = {1,2,3,4,5,6,7,8,9,10};
-        int [] tabl2 = {11,12,13,14,15,16,17,18,19,20,21};
+//        int [] tabl6 = new int [5];
+//
+//        for(int i = 0 ; i < tabl6.length; i++){
+//            System.out.println("Write your number ...");
+//            tabl6[i] = getInt();
+//        }
+//        for (int i = 0; i < tabl6.length; i++){
+//            int result = 1;
+//            for(int j = tabl6[i] ; j > 0; j --){
+//                result *= j;
+//            }System.out.println(" Factorial " + tabl6[i] + " is " + result + ".");
+//        }
+//
+//        System.out.println();
 
-        for(int i = 0; i < tabl1.length; i += 2){
-            System.out.print(tabl1[i] + " ");
-        }
-        System.out.println();
-        for(int i = 0; i < tabl2.length; i += 2){
-            System.out.print(tabl2[i] + " ");
-        }
+        // 6.9.7
 
-        System.out.println();
 
-        // 6.9.2
-        int maxValue = tabl1[0];
-        for (int i : tabl1){
-            if (maxValue < i){
-                maxValue = i;
+        String[] tabl7 = {"one", "two", "three", "four", "five"};
+        String[] tabl8 = {"one", "two", "three", "four", "five"};
+
+        boolean compareL = tabl7.length == tabl8.length;
+        boolean compareC = false;
+
+
+        if (compareL) {
+
+            for (int i = 0; i < tabl7.length; i++) {
+
+                if (tabl7[i] == tabl8[i]) {
+                    compareC = true;
+                } else {
+                    compareC = false;
+                }
             }
-        }
-        System.out.println(maxValue + " is the biggest number");
-
-        System.out.println();
-
-        // 6.9.3
-
-        String [] tabl3 = {"ala", "ola", "bartek", "tomek"};
-
-        for(String i : tabl3){
-            System.out.println(i.toUpperCase() + " ");
+        } else {
+            System.out.println("Arrays are not he same.");
         }
 
-        System.out.println();
-
-        // 6.9.3
-
-        String [] tabl4 = {"ala", "ma", "kota", "i", "psa"};
-
-        for (int i = tabl4.length - 1; i >= 0 ; i--){
-            for(int j = tabl4[i].length() - 1; j >= 0; j--){
-                System.out.print(tabl4[i].charAt(j));
-            }
-            System.out.print(" ");
-        }
-
-        System.out.println();
-
-        // 6.9.4
-
-        int [] tabl5 = new int [8];
-
-        for(int i = 0 ; i < tabl5.length; i++){
-            System.out.println("Write your number ...");
-            tabl5[i] = getInt();
-        }
+        String result = compareC ? "Are the same" : "Are not the same";
+        System.out.println(result);
 
     }
 
-    static int getInt(){
+
+    static int getInt() {
         return new Scanner(System.in).nextInt();
     }
 }
